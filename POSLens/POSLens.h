e9d5849817e76892f2711c0bb2f09de9e8981bb9
Dmitry Golubev <dmitry.golubev@vk.team>
2023-10-06 19:31:33 +0300

    добавил схему

---
+++ POSLens/POSLens.h
@@ -0,0 +1,29 @@
+//
+//  POSLens.h
+//  POSLens
+//
+//  Created by dmitry.golubev on 06.10.2023.
+//  Copyright © 2023 Pavel Osipov. All rights reserved.
+//
+
+#import <POSLens/Foundation/Foundation.h>
+#import <POSLens/NSError+POSLens.h>
+#import <POSLens/POSLens.h>
+#import <POSLens/POSLensValue.h>
+#import <POSLens/POSEphemeralValueStore.h>
+#import <POSLens/POSPersistentValueStore.h>
+#import <POSLens/POSFileValueStore.h>
+#import <POSLens/POSKeychainValueStore.h>
+#import <POSLens/POSUserDefaultsValueStore.h>
+#import <POSLens/POSValueStore.h>
+#import <POSLens/POSEquality.h>
+
+//! Project version number for POSLens.
+FOUNDATION_EXPORT double POSLensVersionNumber;
+
+//! Project version string for POSLens.
+FOUNDATION_EXPORT const unsigned char POSLensVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POSLens/PublicHeader.h>
+
+
